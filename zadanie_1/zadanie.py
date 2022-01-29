89f6825015638cb36df85b43f2f2306efd7bf21b
KobrzAnd <andrzej.kobrzycki@orange.com>
2022-01-29 13:14:14 +0100

    Komunikat przeniesiony do zmiennej

--- zadanie_1/zadanie.py
+++ zadanie_1/zadanie.py
@@ -1,4 +1,3 @@
-
 def error_handler(func):
     def wrapper():
         try:
@@ -23,16 +22,17 @@ def drukuj_oprocentowanie():
                 1.577035247, -0.292781443, 2.48619659, 0.267110318, 1.417952672, 1.054243267,
                 1.480520104, 1.577035247, -0.07742069, 1.165733399, -0.404186718, 1.499708521)
 
+    komunikat = "Twoja pozostała kwota kredytu to {:.6f}, to {:.6f} mniej niż w poprzednim miesiącu."
+
     for i in inflacja:
         poprzedni = kredyt
-        kredyt = (1+((i+procent)/1200))*kredyt-rata
+        kredyt = (1 + ((i+procent) / 1200)) * kredyt-rata
         if kredyt <= 0:
-            print("Twoja pozostała kwota kredytu to {:.6f}, to {:.6f} mniej niż w poprzednim miesiącu."
-                  .format(0.0, poprzedni))
+            print(komunikat.format(0.0, poprzedni))
             break
         else:
-            print("Twoja pozostała kwota kredytu to {:.6f}, to {:.6f} mniej niż w poprzednim miesiącu."
-                  .format(kredyt, poprzedni-kredyt))
+            print(komunikat.format(kredyt, poprzedni - kredyt))
+
 
 if __name__ == "__main__":
     drukuj_oprocentowanie()
