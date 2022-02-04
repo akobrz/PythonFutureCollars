60ce654e26f02097548379318c5991eb20e11b36
Andrzej Kobrzycki <andrzej.kobrzycki@gmail.com>
2022-02-04 18:51:38 +0100

    Poprawa algorytmu

--- zadanie_2_2/zadanie.py
+++ zadanie_2_2/zadanie.py
@@ -2,20 +2,16 @@ def obsluga_bledow(func):
     def wrapper():
         try:
             func()
-        except ValueError:
-            print("Błąd. Brak danych lub niepoprawne dane")
         except AttributeError:
             print("Błąd. Nieprawidłowa wartość parametru")
-        except RecursionError:
-            print("Nie wysłano żadnej paczki")
         return
     return wrapper
 
 
 @obsluga_bledow
 def oblicz_collatza():
-    min_dlugosc, maks_dlugosc, liczba = 0, 100, int(input("Podaj liczbę naturalną, większą od 0 i mniejszą od 100: "))
-    if maks_dlugosc < liczba or liczba <= min_dlugosc:
+    min_dlugosc, maks_dlugosc, liczba = 1, 100, int(input("Podaj liczbę naturalną, większą od 0 i mniejszą od 100: "))
+    if maks_dlugosc < liczba or liczba < min_dlugosc:
         raise AttributeError
     print(f"Długość ciągu Collatza dla liczby {liczba} wynosi: {oblicz(liczba)}")
 
