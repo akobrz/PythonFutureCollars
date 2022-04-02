8aa5449d2c095f9e28b5c462622b5cdd3ea07d1c
DESKTOP-LKP8NN7\andrz <andrzej.kobrzycki@gmail.com>
2022-04-02 20:10:55 +0200

    New version

---
+++ zadanie_8/weather.py
@@ -0,0 +1,148 @@
+import json
+import requests
+import sys
+import time
+from datetime import datetime
+from datetime import timedelta
+
+class WeatherForecast:
+	def __init__(self, key):
+		self.key = key
+		self.fn = "records.txt"
+		self._index = 0
+
+	def __getitem__(self, date2check):
+		self.w = Weather((self.key, date2check))
+		answer = None
+		try:
+			answer = self.w.prepare_answer()
+		except self.w.my_exception:
+			pass
+		return answer
+
+	def items(self):
+		return (row for row in open(self.fn, "r"))
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		f = [row.split(",")[0] for row in open(self.fn, "r")]
+		if self._index < len(f):
+			result = f[self._index]
+			self._index += 1
+			return result
+		raise StopIteration
+
+class Weather:
+
+	class my_exception(Exception):
+		def __init__(self):
+			print("Nie wiem\n")
+
+	def __init__(self, args):
+		if len(args) == 2:
+			self.key, self.date2check = args
+		elif len(args) == 1:
+			self.key, self.date2check = args[0], (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
+		self.today = datetime.now().strftime('%Y-%m-%d')
+		self.lat = "51.79918186247448"
+		self.lon = "19.44995246414342"
+		self.fn = "records.txt"
+
+	def convert_date2unix(self, std_date):
+		dt = [int(d) for d in std_date.split("-")]
+		return int(time.mktime(datetime(*dt).timetuple()))
+
+	def convert_unix2date(self, unix_date):
+		return datetime.utcfromtimestamp(int(unix_date)).strftime('%Y-%m-%d')
+
+	def get_forecast(self):
+		url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+		query = {"q": "Lodz,pl", "lat": self.lat, "lon": self.lon, "id": "2172797", "units": "metric", "mode": "JSON", "cnt": 16}
+		header = {"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com", "X-RapidAPI-Key": self.key}
+		req = requests.request("GET", url, headers=header, params=query)
+		if req.status_code == 200:
+			return json.loads(req.text)
+		else:
+			raise self.my_exception()
+
+	def get_historical(self, historical_date):
+		url = "https://community-open-weather-map.p.rapidapi.com/onecall/timemachine"
+		query = {"lat": self.lat, "lon": self.lon, "id": "2172797", "units": "metric", "mode": "JSON", "dt": f"{self.convert_date2unix(self.date2check)}"}
+		header = {"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com", "X-RapidAPI-Key": self.key}
+		req = requests.request("GET", url, headers=header, params=query)
+		if req.status_code == 200:
+			return json.loads(req.text)
+		else:
+			raise self.my_exception()
+
+	def weather_future(self, weather_description):
+		if weather_description is None:
+			return "Nie wiem"
+		elif "rain" in weather_description:
+			return "Będzie padać"
+		else:
+			return "Nie będzie padać"
+
+	def weather_past(self, weather_description):
+		if weather_description is None:
+			return "Nie wiem"
+		elif "rain" in weather_description:
+			return "Padało"
+		else:
+			return "Nie padało"
+
+	def prepare_answer(self):
+		if self.check_in_file():
+			return self.load_from_file()
+		elif self.today > self.date2check:
+			j = self.get_historical(self.date2check)
+			return self.save_to_file(self.date2check, self.weather_past(j["current"]["weather"][0]["description"]))
+		else:
+			for day in self.get_forecast()["list"]:
+				if self.convert_unix2date(day["dt"]) == self.date2check:
+					return self.save_to_file(self.date2check, self.weather_future(day["weather"][0]["description"]))
+			return self.save_to_file(self.date2check, "Nie wiadomo")
+
+	def save_to_file(self, date, info):
+		with open(self.fn, "a") as file:
+			file.write(date + "," + info + "\n")
+		return info + "\n"
+
+	def load_from_file(self):
+		with open(self.fn, "r") as file:
+			for row in file.readlines():
+				if self.date2check in row:
+					return row.split(",")[1]
+
+	def check_in_file(self):
+		with open(self.fn, "r") as file:
+			for row in file.readlines():
+				if self.date2check in row:
+					return True
+			return False
+
+if __name__ == "__main__":
+	key, date = sys.argv[1:]
+
+	wf = WeatherForecast(key)
+	print(wf[date])
+
+	gen = wf.items()
+	print(next(gen))
+	print(next(gen))
+	print(next(gen))
+	print(next(gen))
+
+	gen = iter(wf)
+	print(next(gen))
+	print(next(gen))
+	print(next(gen))
+	print(next(gen))
+
+
+
+
+
+
